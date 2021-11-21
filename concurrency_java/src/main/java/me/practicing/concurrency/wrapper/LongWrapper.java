e612b49fa9e4fa6e48862f1ab0b182e6413ddfb4
raulmanzas <raulmanzas@gmail.com>
2021-11-21 07:17:58 -0300

    adds race condition example with a LongWrapper

---
+++ concurrency_java/src/main/java/me/practicing/concurrency/wrapper/LongWrapper.java
@@ -0,0 +1,19 @@
+package me.practicing.concurrency.wrapper;
+
+public class LongWrapper {
+    private long l;
+
+    public LongWrapper(long l) {
+        this.l = l;
+    }
+
+    public long getValue() {
+        return l;
+    }
+
+    public synchronized void incrementValue() {
+        // read and write operation that can happen at the same time from different threads, causing a race condition
+        // if there is no lock. The lock used here is the instance itself
+        l = l + 1;
+    }
+}
---
+++ concurrency_java/src/main/java/me/practicing/concurrency/wrapper/RaceCondition.java
@@ -0,0 +1,26 @@
+package me.practicing.concurrency.wrapper;
+
+public class RaceCondition {
+    public static void main(String[] args) throws InterruptedException {
+        LongWrapper longWrapper = new LongWrapper(0l);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1_000; i++) {
+                longWrapper.incrementValue();
+            }
+        };
+
+        Thread[] threads = new Thread[1_000];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(runnable);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+
+        // In case of race condition, value is not 1M
+        System.out.println("Value = " + longWrapper.getValue());
+    }
+}
